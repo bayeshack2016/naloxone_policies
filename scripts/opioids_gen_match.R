42900cbd23ec98fb70b22a5714ea033d0d206626
Eugenia Giraudy <eugeniagiraudy@Eugenias-MacBook-Air.local>
2016-04-24 09:15:48 -0700

    edit script genmatch

--- scripts/opioids_gen_match.R
+++ scripts/opioids_gen_match.R
@@ -3,10 +3,8 @@ library(stringr)
 library(plyr)
 library(ggplot2)
 
-
-setwd("~/Dropbox (YouGov Analytics)/Hack/")
-
 load("./data/opioid.RData")
+
 
 
 opioid$immunity_prescribers_all[opioid$immunity_prescribers_civil %in% 'Yes' & opioid$immunity_prescribers_criminal %in% 'Yes' & opioid$immunity_prescribers_disciplinary %in% 'Yes'] <- 'Yes'
@@ -29,31 +27,21 @@ X = opioid[, c("Percent.Opioid.Claims", "pop13", "age65", "whitealone", "vetshar
 BalanceMat <- opioid[, c("Percent.Opioid.Claims", "pop13", "age65", "whitealone", "vetshare", "povertyline", "Provider.Count")]
 
 
-#opioid$prescribin_permitted_third_party[is.na(opioid$prescribin_permitted_third_party)] <- 'No'
-treat <- I(opioid$prescribin_permitted_third_party == 'Yes')
+vars <- c('immunity_prescribers_all', 'immunity_dispensers_all')
 
-genout <- GenMatch(Tr=treat, X=X, BalanceMatrix=BalanceMat, 
-  estimand="ATE", M=1, max.generations=10, wait.generations=1)
-
-#The outcome variable
-Y=opioid$ratesplit_low
-Y[is.na(Y)] <- 0
-#
-# Now that GenMatch() has found the optimal weights, let's estimate
-# our causal effect of interest using those weights
-#
-mout <- Match(Y=Y, Tr=treat, X=X, estimand="ATE", Weight.matrix=genout)
-summary(mout)
-
-###
-estimate_immunity_prescribers_all <- mout$est
-opioid$estimate_immunity_prescribers_all[opioid$immunity_prescribers_all %in% 'No'] <- opioid$ratesplit_low[opioid$immunity_prescribers_all %in% 'No'] - estimate_immunity_prescribers_all
-opioid$estimate_immunity_prescribers_all[opioid$immunity_prescribers_all %in% 'Yes'] <- opioid$ratesplit_low[opioid$immunity_prescribers_all %in% 'Yes']
-
-estimate_immunity_dispensers_all <- mout$est
-opioid$estimate_immunity_dispensers_all[opioid$immunity_dispensers_all %in% 'No'] <- opioid$ratesplit_low[opioid$immunity_dispensers_all %in% 'No'] - estimate_immunity_dispensers_all
-opioid$estimate_immunity_dispensers_all[opioid$immunity_dispensers_all %in% 'Yes'] <- opioid$ratesplit_low[opioid$immunity_dispensers_all %in% 'Yes']
-
-estimate_prescribin_permitted_third_party <- mout$est
-opioid$estimate_prescribin_permitted_third_party[opioid$prescribin_permitted_third_party %in% 'No'] <- opioid$ratesplit_low[opioid$prescribin_permitted_third_party %in% 'No'] - estimate_prescribin_permitted_third_party
-opioid$estimate_prescribin_permitted_third_party[opioid$prescribin_permitted_third_party %in% 'Yes'] <- opioid$ratesplit_low[opioid$prescribin_permitted_third_party %in% 'Yes']
+for(var in vars){
+  treat <- I(opioid[[var]] == 'Yes')
+  genout <- GenMatch(Tr=treat, X=X, BalanceMatrix=BalanceMat, 
+    estimand="ATE", M=1, max.generations=10, wait.generations=1)
+  #The outcome variable
+  Y=opioid$ratesplit_low
+  Y[is.na(Y)] <- 0
+  # Now that GenMatch() has found the optimal weights, let's estimate
+  # our causal effect of interest using those weights
+  #
+  mout <- Match(Y=Y, Tr=treat, X=X, estimand="ATE", Weight.matrix=genout)
+  print(summary(mout))
+  estimate <- mout$est
+  opioid[[paste0('estimate_', var)]][opioid[[var]] %in% 'No'] <- opioid$ratesplit_low[opioid[[var]] %in% 'No'] - estimate
+  opioid[[paste0('estimate_', var)]][opioid[[var]] %in% 'Yes'] <- opioid$ratesplit_low[opioid[[var]] %in% 'Yes']
+}
